83efdee252bb213c8806a59ba4d8e6602ac07f4b
berkantkaya <berkantkaya19_03@hotmail.com>
2022-03-28 13:46:30 +0300

    changed update

--- src/components/PromotionHead.jsx
+++ src/components/PromotionHead.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const PromotionHead = () => {
   return (
-    <div className='hidden md:flex items-center justify-around pt-2'>
-      <div className='flex text-sm items-center space-x-8 tracking-wider text-white'>
-        <span>Telefon: 555 555 55 55</span>
-        <span>Eposta:loremipsum@lorem.com</span>
+    <div className="hidden md:flex items-center justify-around pt-2">
+      <div className="flex text-sm items-center space-x-8 tracking-wider text-white">
+        <span>Telefon: +90 (0232) 336 9733</span>
+        <span>Eposta: bilgi@gokmenzeki.com</span>
       </div>
-      <div className='flex space-x-2 cursor-pointer'>
-        <img src="/assets/images/facebook.png" alt="" />
-        <img src="/assets/images/instagram.png" alt="" />
-        <img src="/assets/images/twitter.png" alt="" />
+      <div className="flex space-x-2 cursor-pointer">
+        <a
+          target="_blank"
+          href=""
+        >
+          <img src="/assets/images/facebook.png" alt="" />
+        </a>
+        <a target="_blank" href="">
+          <img src="/assets/images/instagram.png" alt="" />
+        </a>
+        {/* <img src="/assets/images/twitter.png" alt="" />
         <img src="/assets/images/youtube.png" alt="" />
-        <img src="/assets/images/linkedin.png" alt="" />
+        <img src="/assets/images/linkedin.png" alt="" /> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PromotionHead
+export default PromotionHead;
--- src/screens/Footer.jsx
+++ src/screens/Footer.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-gray-100 flex items-center justify-between md:p-5'>
-      <img className='w-32 md:w-auto' src="/assets/images/propino-sarı-logo.png" alt="" />    
+      <img className='w-32 md:w-auto' src="/assets/images/propino-sarı-logo.png" alt="" />  
+      <a
+          target="_blank"
+          href="https://nerasocial.com/"
+        >  
       <img className='w-32 md:w-auto mt-4' src="/assets/images/made-by-nera.png" alt="" />    
+      </a>
     </div>
   )
 }
